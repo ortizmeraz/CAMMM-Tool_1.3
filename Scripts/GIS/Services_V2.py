d066d79e4c530d6b430565da899f707ed039bab5
Omar Ortiz Meraz <omar@ortizmeraz.com>
2022-10-18 12:31:17 -0400

    Service calculation

---
+++ Scripts/GIS/Services_V2.py
@@ -0,0 +1,62 @@
+
+
+def FullNum(x:int,TotalX:int)->str:
+    StrTot=str(TotalX)
+    Top=1
+    Exit=str(x)
+    for i in StrTot:
+        Top*=10
+    Diff=Top-x
+    for i in str(Diff):
+        Exit='0'+Exit
+    return (Exit)
+
+def Main(PathNodes,ProcPath):
+    BufferSize={"Cluster":400,"Hub":800}
+    NodeLayers=QgsVectorLayer(PathNodes,"ogr")
+
+    R=0
+    for i in NodeLayers.getFeatures():
+        R+=1
+
+
+    for i in NodeLayers.getFeatures():
+        record=i.attributes()
+        print(record)
+        print(record[9])
+        FiD=record[0]
+
+        print("extractbyattribute")
+        paramsExtract={'INPUT':'C:\\Users\\Omar\\Desktop\\CAMMMM\\NewNodes\\Montreal_Nodes.gpkg|layername=Montreal_Nodes',
+        'FIELD':'fid',
+        'OPERATOR':0,
+        'VALUE':'1',
+        'OUTPUT':'C:/Users/Omar/Desktop/CAMMMM/Processing/TempNode/Node'+FullNum(x=int(FiD),TotalX=R)+'.gpkg'}
+
+        processing.run("native:extractbyattribute", paramsExtract)
+        # break
+
+        print("Creation of the buffer")
+        paramsExtract={'INPUT':'C:/Users/Omar/Desktop/CAMMMM/Processing/TempNode/Node'+FullNum(x=int(FiD),TotalX=R)+'.gpkg',
+        'DISTANCE':100,
+        'SEGMENTS':5,
+        'END_CAP_STYLE':0,
+        'JOIN_STYLE':0,
+        'MITER_LIMIT':2,
+        'DISSOLVE':False,
+        'OUTPUT':'C:/Users/Omar/Desktop/CAMMMM/Processing/Buffer/Buffer_'+FullNum(x=int(FiD),TotalX=R)+'.gpkg'}
+        processing.run("native:buffer", paramsExtract)
+
+
+        print("Clipp of services")
+        paramExtract={'INPUT':'C:/Users/Omar/Desktop/CAMMMM/Services_wCat_UTMwgs84/Services_wCat_UTMwgs84.shp',
+        'OVERLAY':'C:/Users/Omar/Desktop/CAMMMM/Processing/Buffer/Buffer_'+FullNum(x=int(FiD),TotalX=R)+'.gpkg',
+        'OUTPUT':'C:/Users/Omar/Desktop/CAMMMM/Processing/Clip/Clip_'+FullNum(x=int(FiD),TotalX=R)+'.gpkg'}
+        processing.run("native:clip",paramExtract )
+
+
+
+
+BasePath=r"C:\Users\Omar\Desktop\CAMMMM\NewNodes\Montreal_Nodes.gpkg"
+ProcFolderPath=r"C:/Users/Omar/Desktop/CAMMMM/Processing"
+Main(PathNodes=BasePath,ProcPath=ProcFolderPath)
